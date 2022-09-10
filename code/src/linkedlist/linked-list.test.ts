fa6221cc903f8f75ae2e30bd256a63843abc54e8
Sifat Hossain <80267830+h-sifat@users.noreply.github.com>
2022-09-10 22:28:51 +0600

    exercise: implement singly linked list

---
+++ code/src/linkedlist/linked-list.test.ts
@@ -0,0 +1,234 @@
+import { LinkedList } from "./linked-list";
+
+let list: LinkedList<number>;
+
+beforeEach(() => {
+  list = new LinkedList<number>();
+});
+
+describe("Constructor", () => {
+  it(`inerts all the items of the array provided to the constructor`, () => {
+    const array = [1, 2, 3];
+    const myList = new LinkedList<number>(array);
+    expect(myList.toArray()).toEqual(array);
+  });
+});
+
+describe("addFirst", () => {
+  it("add the first item in the list", () => {
+    expect(list.size).toBe(0);
+
+    list.addFirst(1);
+    expect(list.size).toBe(1);
+
+    expect(list.toArray()).toEqual([1]);
+  });
+
+  it("adds the item at the first if link is not empty", () => {
+    list.addFirst(1);
+    list.addFirst(2);
+
+    expect(list.size).toBe(2);
+    expect(list.toArray()).toEqual([2, 1]);
+  });
+});
+
+describe("addLast", () => {
+  it(`adds the first item in the list if it empty`, () => {
+    expect(list.size).toBe(0);
+
+    list.addLast(1);
+    expect(list.size).toBe(1);
+
+    expect(list.toArray()).toEqual([1]);
+  });
+
+  it(`adds items at the last if list is non_empty`, () => {
+    list.addLast(1);
+    list.addLast(2);
+
+    expect(list.size).toBe(2);
+    expect(list.toArray()).toEqual([1, 2]);
+  });
+});
+
+describe("toArray", () => {
+  it.each([{ array: [1, 2, 3] }, { array: [1] }, { array: [] }])(
+    "converts the non_empty list $array into an array",
+    ({ array }) => {
+      array.forEach((item) => list.addLast(item));
+
+      const listArray = list.toArray();
+      expect(listArray).toHaveLength(array.length);
+      expect(listArray).toEqual(array);
+    }
+  );
+});
+
+describe("indexOf", () => {
+  it.each([
+    {
+      items: [],
+      searchItem: 1,
+      result: -1,
+    },
+    {
+      items: [1],
+      searchItem: 1,
+      result: 0,
+    },
+    {
+      items: [1, 2, 3],
+      searchItem: 3,
+      result: 2,
+    },
+    {
+      items: [1, 2, 3],
+      searchItem: 2,
+      result: 1,
+    },
+  ])(
+    `($items).indexOf($searchItem) = $result`,
+    ({ items, searchItem, result }) => {
+      items.forEach((item) => list.addLast(item));
+
+      expect(list.indexOf(searchItem)).toBe(result);
+    }
+  );
+});
+
+describe("contains", () => {
+  it.each([
+    {
+      items: [],
+      searchItem: 1,
+      result: false,
+    },
+    {
+      items: [1],
+      searchItem: 1,
+      result: true,
+    },
+    {
+      items: [1, 2, 3],
+      searchItem: 3,
+      result: true,
+    },
+    {
+      items: [1, 2, 3],
+      searchItem: 2,
+      result: true,
+    },
+  ])(
+    `($items).contains($searchItem) = $result`,
+    ({ items, searchItem, result }) => {
+      items.forEach((item) => list.addLast(item));
+      expect(list.contains(searchItem)).toBe(result);
+    }
+  );
+});
+
+describe("deleteFirst", () => {
+  it(`returns null if the list is empty`, () => {
+    expect(list.deleteFirst()).toBeNull();
+  });
+
+  it(`deletes and returns the first item`, () => {
+    const array = [1, 2, 3];
+    array.forEach((n) => list.addLast(n));
+
+    expect(list.size).toBe(array.length);
+
+    const firstItem = list.deleteFirst();
+
+    expect(list.size).toBe(array.length - 1);
+    expect(firstItem).toBe(array[0]);
+
+    expect(list.toArray()).toEqual(array.slice(1));
+  });
+
+  it(`deletes the first item if the list has only one item`, () => {
+    const insertedItem = 2;
+    list.addLast(insertedItem);
+
+    const deletedItem = list.deleteFirst();
+
+    expect(deletedItem).toBe(insertedItem);
+    expect(list.size).toBe(0);
+  });
+});
+
+describe("deleteLast", () => {
+  it(`returns null if the list is empty`, () => {
+    expect(list.deleteLast()).toBeNull();
+  });
+
+  it(`deletes the first item if the list has only one item`, () => {
+    const insertedItem = 2;
+    list.addLast(insertedItem);
+
+    const deletedItem = list.deleteLast();
+
+    expect(deletedItem).toBe(insertedItem);
+    expect(list.size).toBe(0);
+  });
+
+  it(`deletes and returns the last item`, () => {
+    const array = [1, 2, 3];
+    array.forEach((n) => list.addLast(n));
+
+    expect(list.size).toBe(array.length);
+
+    const lastItem = list.deleteLast();
+
+    expect(list.size).toBe(array.length - 1);
+    expect(lastItem).toBe(array[array.length - 1]);
+
+    expect(list.toArray()).toEqual(array.slice(0, -1));
+
+    expect(list.peekLast()).toBe(array[array.length - 2]);
+  });
+});
+
+describe("peekFirst", () => {
+  it(`returns null if the list is empty`, () => {
+    expect(list.peekFirst()).toBeNull();
+  });
+
+  it(`returns the first item without deleting it`, () => {
+    const insertedItem = 1;
+    list.addFirst(insertedItem);
+
+    expect(list.peekFirst()).toBe(insertedItem);
+    expect(list.size).toBe(1);
+  });
+});
+
+describe("peekLast", () => {
+  it(`returns null if the list is empty`, () => {
+    expect(list.peekLast()).toBeNull();
+  });
+
+  it(`returns the first item without deleting it`, () => {
+    const insertedItem = 1;
+    list.addLast(insertedItem);
+
+    expect(list.peekLast()).toBe(insertedItem);
+    expect(list.size).toBe(1);
+  });
+});
+
+describe("at", () => {
+  it(`returns null if the list is empty`, () => {
+    expect(list.at(1)).toBeNull();
+  });
+
+  it(`returns the item if found`, () => {
+    const array = [1, 2, 3];
+    array.forEach((n) => list.addLast(n));
+
+    array.forEach((item, index) => {
+      expect(list.at(index)).toBe(item);
+    });
+  });
+});
---
+++ code/src/linkedlist/linked-list.ts
@@ -0,0 +1,183 @@
+export interface SinglyLinkedList<Type> {
+  get size(): number;
+  peekLast(): Type | null;
+  peekFirst(): Type | null;
+  addLast(value: Type): void;
+  addFirst(value: Type): void;
+  indexOf(value: Type): number;
+  at(index: number): Type | null;
+  contains(value: Type): boolean;
+  deleteLast(value: Type): Type | null;
+  deleteFirst(value: Type): Type | null;
+}
+
+class Node<Type> {
+  public next: Node<Type> | null = null;
+  constructor(public value: Type, next: Node<Type> | null = null) {
+    if (next) this.next = next;
+  }
+}
+
+export class LinkedList<Type> implements SinglyLinkedList<Type> {
+  #forEachBreakSignal = Symbol();
+  #size = 0;
+  #first: Node<Type> | null = null;
+  #last: Node<Type> | null = null;
+
+  constructor(items?: Type[]) {
+    if (items) for (const item of items) this.addLast(item);
+  }
+
+  #addTheFirstItem(node: Node<Type>) {
+    this.#first = node;
+    this.#last = this.#first;
+    this.#size = 1;
+  }
+
+  peekFirst() {
+    return this.#first ? this.#first.value : null;
+  }
+
+  peekLast() {
+    return this.#last ? this.#last.value : null;
+  }
+
+  addFirst(value: Type) {
+    const node = new Node<Type>(value);
+
+    if (!this.#first) {
+      this.#addTheFirstItem(node);
+      return this;
+    }
+
+    node.next = this.#first;
+    this.#first = node;
+    this.#size++;
+
+    return this;
+  }
+
+  addLast(value: Type) {
+    const node = new Node<Type>(value);
+
+    if (!this.#first) {
+      this.#addTheFirstItem(node);
+      return this;
+    }
+
+    this.#last!.next = node;
+    this.#last = node;
+
+    this.#size++;
+
+    return this;
+  }
+
+  deleteFirst(): Type | null {
+    if (!this.#first) return null;
+    if (this.#size === 1) return this.#deleteTheOnlyItemInList();
+
+    const value = this.#first.value;
+
+    const newFirstNode = this.#first.next;
+    this.#first.next = null;
+    this.#first = newFirstNode;
+
+    this.#size--;
+
+    return value;
+  }
+
+  #deleteTheOnlyItemInList() {
+    const value = this.#first!.value;
+
+    this.#first = null;
+    this.#last = null;
+    this.#size--;
+
+    return value;
+  }
+
+  deleteLast(): Type | null {
+    if (!this.#first) return null;
+    if (this.#size === 1) return this.#deleteTheOnlyItemInList();
+
+    const prevNodeOfTheLastNode = (() => {
+      let current = this.#first;
+      while (current.next?.next) {
+        current = current.next;
+      }
+      return current;
+    })();
+
+    const value = prevNodeOfTheLastNode.next!.value;
+
+    prevNodeOfTheLastNode.next = null;
+    this.#last = prevNodeOfTheLastNode;
+
+    this.#size--;
+
+    return value;
+  }
+
+  indexOf(value: Type) {
+    let index = -1;
+
+    this.#forEach((item, _index) => {
+      if (item !== value) return;
+
+      index = _index;
+      // break the forEach loop because we've found the searchItem
+      return this.#forEachBreakSignal;
+    });
+
+    return index;
+  }
+
+  contains(value: Type) {
+    return this.indexOf(value) !== -1;
+  }
+
+  toArray() {
+    if (!this.#first) return [];
+
+    const array = new Array(this.#size);
+
+    this.#forEach((item, index) => {
+      array[index] = item;
+    });
+
+    return array;
+  }
+
+  #forEach(callback: (item: Type, index: number) => void | Symbol) {
+    if (!this.#first) return;
+
+    let index = 0;
+    let current: Node<Type> | null = this.#first;
+    while (current) {
+      const res = callback(current.value, index++);
+
+      // break the loop if callback returns the break signal symbol
+      if (res == this.#forEachBreakSignal) return;
+      current = current.next;
+    }
+  }
+
+  at(index: number) {
+    let value: Type | null = null;
+
+    this.#forEach((currentValue, currentIndex) => {
+      if (currentIndex !== index) return;
+
+      value = currentValue;
+      return this.#forEachBreakSignal;
+    });
+
+    return value;
+  }
+
+  get size() {
+    return this.#size;
+  }
+}
